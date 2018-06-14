7c1fa4303acc55c6d9b0a4284c130b45a70ef37c
Kevinlpd <davidkevinit@gmail.com>
2018-06-14 18:12:20 +0700

    Edit structure of shell view

--- src/controllers/ShellController.java
+++ src/controllers/ShellController.java
@@ -8,20 +8,12 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.List;
 
 import fxml.ShellView;
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.ProgressIndicator;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TreeView;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import models.TargetModel;
 import utilities.PopupUtils;
 
@@ -32,51 +24,20 @@ import utilities.PopupUtils;
 public class ShellController extends Thread{
 	private TargetModel target;
 	private ShellView view;
-	private TableView table_view;
-	private ImageView loading;
-	private TreeView<String> tree_view;
-	private Tab tab_session;
 
-	/*
-	 * [Shellmanager -> tab_session] -> [sub_tab_pane -> tab_explorer -> anchor pane] -> [ tree_view
-	 * 																					-> table_view -> cl_name ...
-	 * 																					-> progress_bar ]
-	 */
+	
 	public ShellController(String id, TabPane Shellmanager){		
 		this.target = new TargetModel().getTargetById(id);
-		this.view = new ShellView();
-		// Load view
-		
-		// Tree view
-		this.tree_view = view.create_treeview();
-		
-		// Table view
-		TableColumn cl_name = view.create_table_column("Name");
-		TableColumn cl_date = view.create_table_column("Date Modified");
-		TableColumn cl_type = view.create_table_column("Type");
-		TableColumn cl_size = view.create_table_column("Size");
-		this.table_view = view.create_table_view(FXCollections.observableArrayList(cl_name, cl_date, cl_type, cl_size));
-		
-		// Icon loading
-		this.loading = view.create_loading();
-		
-		// Tab
-		AnchorPane anchorpane = view.create_anchorpane(Arrays.asList(tree_view, this.loading, table_view));
-		Tab tab_explorer = view.create_tab("Explorer", anchorpane);
-		TabPane sub_tab_pane = view.create_tabpane(Arrays.asList(tab_explorer));
-		
-
-		
-		// session
-		this.tab_session = view.create_tab_session(id, sub_tab_pane);
-		Shellmanager.getTabs().add(this.tab_session);
-		
+		this.view = new ShellView(target, Shellmanager);
 	}
 	
 	@Override
 	public void run() {
-		sendGet();
-		System.out.print(123123);
+
+		//sendGet();
+		List disks = Arrays.asList("C", "D", "E", "F");
+		this.view.getLoading().setVisible(false);
+		this.view.add_item_to_tree_view(this.view.getRoot(), disks);
 		
 	}
 	
@@ -119,7 +80,7 @@ public class ShellController extends Thread{
 	public void exit() {
 		Thread.currentThread().interrupt();
 		Platform.runLater(()->{
-			this.tab_session.getTabPane().getTabs().remove(tab_session);
+			this.view.getTab_session().getTabPane().getTabs().remove(this.view.getTab_session());
 		});
 	}
 	
--- src/fxml/ShellView.java
+++ src/fxml/ShellView.java
@@ -3,31 +3,28 @@
  */
 package fxml;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Observable;
 
-import controllers.ShellController;
-import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.Event;
-import javafx.event.EventType;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.geometry.Side;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import models.TargetModel;
 import utilities.MyScreen;
@@ -37,7 +34,76 @@ import utilities.MyScreen;
  *
  */
 public class ShellView {
+	private Tab tab_session;
+	private TreeView tree_view;
+	private ImageView loading;
 	
+	public ShellView() {}
+	
+	/*
+	 * [Shellmanager -> tab_session] -> [sub_tab_pane -> tab_explorer -> anchor pane] -> 
+	 * 		[	-> Current path
+	 *			-> title
+	 *			-> tree_view
+	 * 			-> table_view -> cl_name ...
+	 * 			-> progress_bar 
+	 * 		]
+	 */
+	public ShellView(TargetModel target, TabPane Shellmanager) {
+		// Current path
+		TextField current_path = this.create_current_path();
+		
+		// Title
+		Button title = this.create_title();
+		
+		// Tree view
+		this.tree_view = this.create_treeview("linux");	
+		
+		// Table view
+		TableColumn cl_name = this.create_table_column("Name", 200.0, 500.0, 389.0);
+		TableColumn cl_date = this.create_table_column("Date Modified", 10.0, 150.0, 150.0);
+		TableColumn cl_type = this.create_table_column("Type", 10.0, 90.0, 150.0);
+		TableColumn cl_size = this.create_table_column("Size", 10.0, 90.0, 150.0);
+		TableView table_view = this.create_table_view(FXCollections.observableArrayList(cl_name, cl_date, cl_type, cl_size));
+		
+		// Icon loading
+		loading = this.create_loading();
+		
+		// Tab
+		AnchorPane anchorpane = this.create_anchorpane(Arrays.asList(loading, current_path, this.tree_view, table_view, title));
+		Tab tab_explorer = this.create_tab("Explorer", anchorpane);
+		TabPane sub_tab_pane = this.create_tabpane(Arrays.asList(tab_explorer));
+				
+		// session
+		this.tab_session = this.create_tab_session(target.getId()+"", sub_tab_pane);
+		Shellmanager.getTabs().add(this.tab_session);
+				
+	}
+	// ========================= ACTIONS ================================ //
+	
+	public void add_item_to_tree_view(TreeItem parrent, List<String> item_list) {
+		for (String item : item_list) {
+			this.tree_view.getTreeItem(
+				this.tree_view.getTreeItemLevel(parrent)
+			).getChildren().add(new TreeItem<String>(item));
+		}
+	}
+	
+	
+	// ======================== GETTERS & SETTERS ======================= //
+	
+	public Tab getTab_session() {
+		return tab_session;
+	}
+	public ImageView getLoading() {
+		return loading;
+	}
+	public TreeItem<String> getRoot(){
+		return this.tree_view.getRoot();
+	}
+	
+	// ================================ NEW WINDOWS ===================== //
+
 	/*
 	 * Create new windows manage shell
 	 * PARAMETER:
@@ -115,7 +181,7 @@ public class ShellView {
 		return t;
 	}
 	/*
-	 * 
+	 * Contain by tabs. It contains tree view, table view, ...
 	 */
 	public AnchorPane create_anchorpane(List<Node> item_list) {
 		AnchorPane ap = new AnchorPane();
@@ -124,30 +190,65 @@ public class ShellView {
 		}		
 		return ap;
 	}
+	
+	// ======================== TREE VIEW =============================== //
+		
 	/*
-	 * 
+	 * Create view for display all disks, folder tree
 	 */
-	public TreeView<String> create_treeview() {
-		TreeView<String> tv = new TreeView<>();
+	public TreeView<String> create_treeview(String OS) {
+		TreeItem<String> root;
+		if (OS.equals("windows")) {
+			root = new TreeItem<String>("This PC");
+		}else { // Linux
+			root = new TreeItem<String>("/");
+		}
+		root.setExpanded(true);
+				
+		TreeView tv = new TreeView(root);
 		tv.setPrefHeight(623);
 		tv.setPrefWidth(252);
+		tv.setLayoutY(32);
+		tv.setPadding(new Insets(25, 0, 0, 0));
 		return tv;
 	}
 	/*
-	 * 
+	 * Display title "Disks"
 	 */
-	public TableColumn create_table_column(String name) {
+	public Button create_title() {
+		Button bt = new Button("Disks");
+		bt.setStyle("-fx-font-weight: bold");
+		bt.setPrefWidth(252);
+		bt.setPrefHeight(23);
+		bt.setLayoutY(32);
+		return bt;
+	}
+		
+	// ======================== TABLE VIEW ============================== //
+	
+	/*
+	 * Create column for table be call in create_table_view();
+	 * PARAMETER:
+	 * * name: display column name
+	 */
+	public TableColumn create_table_column(String name, Double min_width, Double max_width, Double pref_width) {
 		TableColumn tc = new TableColumn(name);
+		tc.setPrefWidth(pref_width);
+		tc.setMaxWidth(max_width);
+		tc.setMinWidth(min_width);
 		return tc;
 	}
 	/*
-	 * 
+	 * Create table for view all files and folders ...
+	 * PARAMETERS:
+	 * * cols: list of column name
 	 */
 	public TableView create_table_view(ObservableList<TableColumn> cols) {
 		TableView tbv = new TableView();
 		tbv.setPrefHeight(622);
 		tbv.setPrefWidth(749);
 		tbv.setLayoutX(252);
+		tbv.setLayoutY(32);
 		
 		for (TableColumn col : cols) {
 			tbv.getColumns().add(col);
@@ -155,7 +256,12 @@ public class ShellView {
 		
 		return tbv;
 	}
+		
+	// ======================== ICON LOADING ============================ //
 	
+	/*
+	 * Create dynamic image loading
+	 */
 	public ImageView create_loading() {
 		ImageView img = new ImageView("/resources/Loading.gif");
 		img.setLayoutY(292);
@@ -164,8 +270,22 @@ public class ShellView {
 		img.setFitHeight(40);
 		return img;
 	}
+	
+	// ======================== TEXT FIELD PATH ========================= //
+	
+	/*
+	 * Field current path in system
+	 */
+	public TextField create_current_path() {
+		TextField tf = new TextField();
+		tf.setPromptText("Current Path");
+		tf.setPrefWidth(550);
+		tf.setLayoutX(211);
+		tf.setLayoutY(2);
+		return tf;
+	}
 
-
-
+	// ======================================== //
+	
 
 }
